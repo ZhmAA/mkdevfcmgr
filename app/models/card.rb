410f5cc28c124f14b48bf70c3cc1551387da40cb
ZhmAA <azhm@ukr.net>
2015-11-16 01:12:32 +0200

    add create edit and destroy cards, validation, simple_form

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,16 @@
 class Card < ActiveRecord::Base
+  before_save :auto_date
+  validates :original_text, presence: true
+  validates :translated_text, presence: true
+  validate :check_unique
+
+  def auto_date
+  	puts Time.current + 3.days
+  end
+
+  def check_unique
+  	original_text = original_text.to_s.downcase.strip
+  	translated_text = translated_text.to_s.downcase.strip
+  	errors.add(:original_text, "can't be the same as translated_text") if original_text == translated_text
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   resources :cards
 
+  get 'cards/:id/delete' => 'cards#delete', :as => :cards_delete
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
