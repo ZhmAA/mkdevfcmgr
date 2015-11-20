af1bc055d1055d1a11a941f767cff67516d77137
ZhmAA <azhm@ukr.net>
2015-11-20 02:01:54 +0200

    add script to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'open-uri'
+require 'nokogiri'
+
+doc = Nokogiri::HTML(open("http://www.languagedaily.com/learn-german/vocabulary/most-common-german-words-2"))
+
+doc.xpath('//table//tr[@class="rowA" or @class="rowB"]').each do |word| 
+  original = word.css(".bigLetter").text
+  translated = word.css("td:nth-child(3)").text
+  Card.create(original_text: original, translated_text: translated, review_date: Time.current)
+end
+
