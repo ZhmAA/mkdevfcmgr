f2660e949bc5f7d55d1fe7192001deff964f49ac
ZhmAA <azhm@ukr.net>
2016-04-04 19:54:39 +0300

    add card methods for task

--- app/models/card.rb
+++ app/models/card.rb
@@ -8,13 +8,38 @@ class Card < ActiveRecord::Base
   validate :check_unique
 
   scope :random_cards, -> { where('review_date <= ?', Time.current).order('RANDOM()') }
-
+  
   def auto_date
     unless self.new_record?
-      self.review_date = Date.current + 3.days
+      self.review_date = Time.current + 3.days
     end
   end
-
+  
+  def change_date(group_num)
+    case group_num
+      when 0
+        Time.current
+      when 1
+        Time.current + 12.hour
+      when 2
+        Time.current + 3.days
+      when 3
+        Time.current + 1.week
+      when 4
+        Time.current + 2.weeks
+      else
+        Time.current + 1.month
+    end
+  end
+  
+  def bad_tries
+    if try_num == 0
+      update_attributes(group_num: group_num - 1, try_num: 3)
+    else
+      update_attributes(try_num: try_num - 1)
+    end
+  end
+  
   def check_unique
     if original_text.downcase == translated_text.downcase
       errors.add(:original_text, "can't be the same as translated_text")
@@ -23,10 +48,13 @@ class Card < ActiveRecord::Base
 
   def check_card(translate)
     if self.original_text == translate
-      self.review_date = Date.current + 3.days
+      self.review_date = change_date(group_num)
+      self.update(review_date: review_date, group_num: group_num + 1, try_num: 3)
       self.save
-    else 
+    else
+      bad_tries
       false
     end
   end
+
 end
