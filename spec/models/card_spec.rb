9e8c287fdaced98dc967593c168fead6a40832a8
ZhmAA <azhm@ukr.net>
2016-04-05 00:04:27 +0300

    add new tests for card model

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -8,21 +8,12 @@ RSpec.describe Card, :type => :model do
   before(:each) do
     login("first@name.com", "123456789")
     @card = create(:card, deck: deck, user: user)
-    @value = Date.current + 3.days
+    @valueone = (Time.current + 12.hour).strftime("%Y-%m-%d %H:%M:%S")
+    @valuetwo = (Time.current + 1.week).strftime("%Y-%m-%d %H:%M:%S")
   end
 
   it "check that original and translated text not equal" do
     expect(@card.original_text).not_to eq(@card.translated_text)
-  end
-  
-  it "check that date change after card update" do
-    @card.auto_date
-    expect(@card.review_date).to eq(@value)
-  end
-  
-  it "check that when card check date change" do
-    @card.check_card(@card.original_text)
-    expect(@card.review_date).to eq(@value)
   end
   
   it "check that input text not equal to original text" do
@@ -31,5 +22,53 @@ RSpec.describe Card, :type => :model do
 
   it "check that input text equal to original text" do
     expect(@card.check_card("original_text")).to be true
+  end
+
+  context "check group switch" do
+    
+    it "check group after first check" do
+      @card.check_card("original_text")
+      expect(@card.group_num).to eq 1
+    end
+
+    it "check switching group" do
+      @card.group_num = 2
+      @card.check_card("original_text")
+      expect(@card.group_num).to eq 3
+    end
+
+    it "rewiev date change after swithing group" do
+      @card.check_card("original_text")
+      expect((@card.review_date).strftime("%Y-%m-%d %H:%M:%S")).to eq(@valueone)
+    end
+
+    it "rewiev date change after swithing group third time" do
+      @card.group_num = 3
+      @card.check_card("original_text")
+      expect((@card.review_date).strftime("%Y-%m-%d %H:%M:%S")).to eq(@valuetwo)
+    end
+  end
+
+  context "check limit tries" do
+    
+    it "check that try down if check false" do
+      @card.check_card("new ot input")
+      expect(@card.try_num).to eq 2
+    end
+
+    before(:each) do
+      @card.group_num = 4
+      @card.try_num = 0
+      @card.check_card("new ot input")
+    end
+
+    it "check what happens with group number when try eq 0" do
+      expect(@card.group_num).to eq 3
+    end
+
+    it "check what happens with try number when try eq 0" do
+      expect(@card.try_num).to eq 3
+    end
+
   end
 end
